f3e9fa8c7267b0feb9cb378b60b78bdb6bb7c285
KyleThie <83198854+KyleThie@users.noreply.github.com>
2023-05-04 13:06:53 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("is it working?");
+
+let myLibrary = [];
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
