ff9f82ed9fcb36ca7cae2a35dc9f0510314866ab
KyleThie <83198854+KyleThie@users.noreply.github.com>
2023-05-05 12:50:26 -0500

    Add image for book cards

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 console.log("is it working?");
-
+let cardContainer = document.getElementById("card-container");
 let myLibrary = [];
+// const oneCard = document.createElement("div");
+// oneCard.innerHTML = "div <br> 5";
+card = document.createElement("div");
+card.className = "testCard";
+card.innerHTML = "fs;ae";
+
+cardContainer.appendChild(card);
+console.log("cardContainer:", cardContainer);
 
 class Book {
   constructor(title, author, pages, read) {
@@ -15,3 +23,20 @@ function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
+
+addBookToLibrary("fsea", "pfoisae", 321, "fwas");
+addBookToLibrary("waga", "aaaaa", 52, "abt");
+
+console.log("myLibrary", myLibrary);
+
+function showBooks() {
+  myLibrary.forEach((book) => {
+    const singleBook = document.createElement("div");
+    singleBook.className = "card";
+    singleBook.innerHTML = `${book.title}`;
+    console.log(singleBook);
+    cardContainer.appendChild(BookTitle);
+  });
+}
+
+showBooks();
