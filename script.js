6938a2c45bc81aa728f1bc369126dba277f67ba2
KyleThie <83198854+KyleThie@users.noreply.github.com>
2023-05-07 21:26:25 -0500

    removeBook function / input field working

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let cardContainer = document.getElementById("card-container");
 let addBtn = document.querySelector("#addBtn");
+let removeBtn = document.querySelector("#removeBtn");
+
+let bookTitle = "";
 titleField = document.querySelector("#title");
 authorField = document.querySelector("#author");
 pageNumField = document.querySelector("#pageNum");
 readField = document.querySelector("#checkBox");
-
+removeField = document.querySelector("#remove");
+let myLibrary = [];
 let newTitle = "";
 let newAuthor = "";
 let pageNums = null;
 let read = null;
+let bookId = -1;
+let bookTitleToDelete = "";
+
+removeField.addEventListener("input", (e) => {
+  // e.preventDefault();
+  bookTitleToDelete = e.target.value;
+  console.log(bookTitleToDelete);
+});
 
 titleField.addEventListener("input", (e) => {
   newTitle = e.target.value;
@@ -34,14 +46,17 @@ addBtn.addEventListener("click", () => {
     read = "Not Read";
   }
   addBookToLibrary(newTitle, newAuthor, pageNums, read);
-  console.log(myLibrary);
+
   showBooks();
   let formById = document.getElementById("formId");
-  // readField.checked is working!!!!
-  console.log(readField.checked);
+
   formById.reset();
 });
-let myLibrary = [];
+
+removeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  removeBook(bookTitleToDelete);
+});
 
 class Book {
   constructor(title, author, pages, read) {
@@ -60,20 +75,73 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("Dracula", "Bram Stoker", 418, "Read");
 addBookToLibrary("Dune", "Frank Herbert", 896, "Not Read");
 
-console.log("myLibrary", myLibrary);
+let singleBook;
 
 function showBooks() {
   myLibrary.forEach((book) => {
+    bookId += 1;
     const singleBook = document.createElement("div");
+    const deleteBtn = document.createElement("button");
+
+    singleBook.appendChild(deleteBtn);
     singleBook.className = "card";
+    singleBookId = bookId;
+
     singleBook.innerHTML = `
+
       <img src='./images/bookIcon.png' alt='Book icon' />
       <h2>${book.title}</h2>
       <h4>${book.author}</h4>
       <h4>${book.pages}</h4>
       <h4>${book.read}</h4>
+      <button>Delete</button>
+      <h4>${singleBookId}</h4>
 `;
-    console.log(singleBook);
+    // singleBook.addEventListener("click", () => {
+    //   const element = document.getElementById(singleBookId);
+    //   element.remove();
+    // });
+    singleBook.setAttribute("id", bookId);
+    // singleBook.addEventListener("click", myFunction(bookId));
+    // console.log("SINGLEBOOKID", singleBookId);
+    // console.log(myLibrary);
+    // console.log(singleBook);
     cardContainer.appendChild(singleBook);
   });
 }
+
+// THIS MIGHT BE KIND OF WORKING???
+
+function removeBook(bookTitleToDelete) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let currentBook = myLibrary[i];
+    console.log("book.title", currentBook.title);
+    console.log("param", bookTitleToDelete);
+    console.log(myLibrary.indexOf(currentBook));
+    // working up to this point - loop and variable seems to be okay
+
+    if (currentBook.title === bookTitleToDelete) {
+      const deletedBookArr = myLibrary.splice(
+        myLibrary.indexOf(currentBook),
+        1
+      );
+      console.log("deletedBOOKARR: ", deletedBookArr);
+    }
+  }
+
+  // console.log(myLibrary);
+  // return myLibrary;
+}
+// removeBook("Dune");
+
+// function deleteBook(toDelete) {
+//   const filtered = myLibrary.filter((book) => {
+//     book.title !== toDelete;
+//   });
+//   return filtered;
+// }
+// removeBook("Dune");
+
+// Try calling removeBook with more / different entries
+// Maybe an issue with how the for loop is set up? myLibrary.length + 1???
+// removeBook("Dune");
